7a9f67d8ce2e003bdc33caa5825be25776d71fac
jerry <ming7435@163.com>
2015-06-04 01:54:53 +0800

    first commit

--- src/com/fuiou/mgr/util/IVRUtil.java
+++ src/com/fuiou/mgr/util/IVRUtil.java
@@ -6,13 +6,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.fuiou.mer.model.TCustmrBusi;
-import com.fuiou.mer.util.FuMerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fuiou.mer.service.TSeqService;
+import com.fuiou.mer.util.MemcacheUtil;
 
 public class IVRUtil {
+	
+	public static Logger logger=LoggerFactory.getLogger(IVRUtil.class);
+	
+	/*****远程数据库主机ip，用户密码******/
+	//192.168.82.15
+	//administrator
+	//china#22222
+	
 
-	private static final String jdbcUrl = "jdbc:sqlserver://192.168.82.15:1433;databaseName=AltiCDR;user=sa;password=22222";
-	private static final String tableNm = "Alti_225661860";
+	private static final String jdbcUrl = "jdbc:sqlserver://192.168.72.106:1433;databaseName=AltiCDR;user=sa;password=22222";
+	private static final String TABLE_NAME = "Alti_230232365";
+	private static final int TASK_ID = 230232365;
+	private static final int ATTEMPTS = 3;//尝试外呼次数
+	private static final int INIT_STATUS = 0;
 
 	private static Connection getConn() {
 		try {
@@ -59,29 +73,30 @@ public class IVRUtil {
 	}
 
 	/**
-	 * 添加语音验证码
-	 * 
-	 * @param mobile_no
-	 *            手机号
-	 * @param code
-	 *            验证码
-	 * @param flag
-	 *            语音标志
+	 * @param mobile:手机号
 	 * @throws Exception
 	 */
-	public static void addIvr(TCustmrBusi custmrBusi) throws Exception{
+	public static void addIvrTask(String mobile) throws Exception{
 		Connection con=null;
 		Statement st=null;
 		ResultSet rs=null;
 		try {
 			con=getConn();
-			for(int i=0;i<10;i++){
-				String sql="insert into "+tableNm+"(id,result1,attempts,taskid,altiphone1,altiextendfield1,altiextendfield2,altiextendfield3) " +
-						"	values("+i+",0,3,225661860,'"+transMobileNo(custmrBusi.getMOBILE_NO())+"','"+FuMerUtil.getCurrTime("MMddHHssmmmm")
-						+"','"+custmrBusi.getACNT_NO()+"','"+custmrBusi.getRESERVED1()+"')";
-				st=con.createStatement();
-				rs=st.executeQuery(sql);
-			}
+			st=con.createStatement();
+//				String querySql = "select count(1) from "+TABLE_NAME+" where result1 in(0,1,21) and taskid="+TASK_ID+" and altiphone1='"+transMobileNo(mobile)+"'";
+//				rs=st.executeQuery(querySql);
+//				if(rs.next()){
+//					if(rs.getInt(1)==0){
+						String insertSql="insert into "+TABLE_NAME+"(id,result1,attempts,taskid,altiphone1) " +
+								"	values("+Long.parseLong(new TSeqService().getId("ivrId", 8, ""))+","+INIT_STATUS+","+ATTEMPTS+","+TASK_ID+",'"+mobile+"')";
+						int i = st.executeUpdate(insertSql);
+						if(i==1){
+							logger.debug("add ivr task success,mobile="+mobile);
+						}else{
+							logger.debug("add ivr task fail,mobile="+mobile);
+						}
+//					}
+//				}
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
@@ -99,30 +114,26 @@ public class IVRUtil {
 	 * @return
 	 */
 	private static String transMobileNo(String mobile_no) {
-//		if (StringUtil.isEmpty(mobile_no) || mobile_no.trim().length() < 7) {
-//			return (mobile_no == null ? "" : mobile_no.trim());
-//		}
-//		boolean isShh = false;
-//		for (String mobileBin : Constants.SHH_MOBILE_BIN_LIST) {
-//			if (StringUtil.isNotEmpty(mobileBin)
-//					&& mobileBin.trim().equals(mobile_no.substring(0, 7))) {
-//				isShh = true;
-//				break;
-//			}
-//		}
-//		if (isShh) {
+		if (StringUtil.isEmpty(mobile_no) || mobile_no.trim().length() < 7) {
+			return (mobile_no == null ? "" : mobile_no.trim());
+		}
+		boolean isShh = false;
+		for (String mobileBin : MemcacheUtil.getShangHaiMobileBin()) {
+			if (StringUtil.isNotEmpty(mobileBin)&& mobileBin.trim().equals(mobile_no.substring(0, 7))) {
+				isShh = true;
+				break;
+			}
+		}
+		if (isShh) {
 			return mobile_no.trim();
-//		} else {
-//			return "0" + mobile_no.trim();
-//		}
+		} else {
+			return "0" + mobile_no.trim();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		TCustmrBusi custmrBusi = new TCustmrBusi();
-		custmrBusi.setMOBILE_NO("13261568767");
-		custmrBusi.setACNT_NO("6227001215050471420");
-		custmrBusi.setRESERVED1("测试商户");
-		addIvr(custmrBusi);
+		addIvrTask("18201868440");
+		addIvrTask("18201868441");
 	}
 
 }
