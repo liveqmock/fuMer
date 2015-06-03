9fedd201912a60bd42a6b2557de13369cfbccbb2
jerry <ming7435@163.com>
2015-06-04 01:54:50 +0800

    first commit

--- src/com/fuiou/mgr/http/util/OrderPayWorker.java
+++ src/com/fuiou/mgr/http/util/OrderPayWorker.java
@@ -3,6 +3,7 @@ package com.fuiou.mgr.http.util;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +43,7 @@ public class OrderPayWorker implements Runnable {
 	@Override
 	public void run() {
 		String str = null;
+		String hangupCode = "DJ0051_DJ0052_DJ0053_DJ0054_DJ0055_DJ0056_DJ0057";//挂断应答码
 		try {
 			BufferedInputStream inputStream = new BufferedInputStream(socket.getInputStream());
 			byte[] buffer = new byte[2048];
@@ -50,9 +52,19 @@ public class OrderPayWorker implements Runnable {
 			logger.debug(str);
 			if (StringUtils.isNotEmpty(str)) {
 				parseMsg(str);
+				TIvrOrderInf order = orderService.getOrder(map.get("OdrDtTm").substring(0, 8),map.get("OdrId"));
+				logger.debug("from vpc msg reponse code:"+ map.get("RspCd"));
 				if (map.get("RspCd") != null && "DJ0000".equals(map.get("RspCd"))) {
 					doPay();
+				}else if(map.get("RspCd") != null && hangupCode.contains(map.get("RspCd"))){//挂断
+					if(null!=order){
+						orderService.updateOrderSt(order.getROW_ID(), TDataDictConst.IVR_ORDER_PAY_OVER, 0);
+					}
+				}else{//其他错
+					logger.debug("other error code:"+map.get("RspCd"));
 				}
+				//响应VPC
+				responseVpc(order.getMOBILE_NO());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,7 +81,7 @@ public class OrderPayWorker implements Runnable {
 		int index = respStr.indexOf("\r\n\r\n");
 		respStr = respStr.substring(index);
 		respStr = VPCDecodeUtil.doCrypt("D", respStr);
-		System.out.println(respStr);
+		logger.debug("respStr:"+respStr);
 		String[] strs = respStr.split("&");
 		for (String s : strs) {
 			String[] ss = s.split("=");
@@ -93,16 +105,15 @@ public class OrderPayWorker implements Runnable {
 		TCustmrBusi custmrBusi = custmrBusiService.selectByAcntAndBusiCd(order.getMCHNT_CD(), TDataDictConst.BUSI_CD_INCOMEFOR,order.getACNT_NO(),CustmrBusiValidator.srcChnlMap.get(CustmrBusiValidator.SRC_CHNL_WEB));
 		BpsUtilBean deductionResultBean = null,paymentResultBean = null;
 		deductionResultBean = BpsTransaction.sendToUpmp(order,map.get("pin"));//扣款结果
+		//扣款结果入库
+		TWebLog webLogDBean = webLogService.saveWebLogD(order, deductionResultBean);//扣款结果入库
+		if (webLogDBean == null){
+			throw new Exception("insert order fail,order num is " + order.getORDER_NO());
+		}else{
+			logger.debug("weblog insert success");
+		}
 		if(BpsTransaction.SUCC_CODE.equals(deductionResultBean.getRespCode())){
-			 //如果扣款成功则立即发一笔付款
-			paymentResultBean = BpsTransaction.cupsPayment(custmrBusi,order.getORDER_AMT()+"");
-			if(BpsTransaction.SUCC_CODE.equals(paymentResultBean.getRespCode())){
-				//扣款结果入库
-				TWebLog webLogDBean = webLogService.saveWebLogD(order, deductionResultBean);//扣款结果入库
-				if (webLogDBean == null)
-					throw new Exception("insert order fail,order num is " + order.getORDER_NO());
-				//付款结果入库
-				webLogService.saveWebLogC(webLogDBean,paymentResultBean);
+			if("C".equals(order.getCONTRACT_FLAG())){//签约
 				//根据支付结果修改协议库状态
 				 boolean flag = CustmrBusiContractUtil.VERIFY_PASS.equals(custmrBusi.getACNT_IS_VERIFY_1());//户名证件号验证状态
 				 custmrBusi.setACNT_IS_VERIFY_2(CustmrBusiContractUtil.VERIFY_PASS);//卡密验证通过
@@ -115,10 +126,31 @@ public class OrderPayWorker implements Runnable {
 				 int rows = custmrBusiService.updateByRowId(custmrBusi);//修改协议库
 				 if(1!=rows){
 					 throw new
-					 	Exception("custmr busi update fail,custmrbuis acnt_no="+custmrBusi.getACNT_NO());
+					 	Exception("contracting....,custmr busi update fail,acnt_no="+custmrBusi.getACNT_NO());
 				 }
-				 //修改掉低级别的签约状态
-				 rows = custmrBusiService.updateRowTp(custmrBusi);
+			}else{
+				custmrBusi.setREC_UPD_USR(custmrBusi.getUSER_NM());//持卡人自己
+				custmrBusi.setREC_UPD_TS(new Date());
+				custmrBusi.setRESERVED2("1");// 标示已解约
+				custmrBusi.setCONTRACT_ST(CustmrBusiContractUtil.CONTRACT_ST_INVALID);//不生效
+				custmrBusi.setACNT_IS_VERIFY_1(CustmrBusiContractUtil.VERIFY_UNPASS);
+				custmrBusi.setACNT_IS_VERIFY_2(CustmrBusiContractUtil.VERIFY_UNPASS);
+				custmrBusi.setACNT_IS_VERIFY_3(CustmrBusiContractUtil.VERIFY_UNPASS);
+				custmrBusi.setACNT_IS_VERIFY_4(CustmrBusiContractUtil.VERIFY_UNPASS);
+				int rows = custmrBusiService.updateByRowId(custmrBusi);
+				 if(1!=rows){
+					 throw new
+					 	Exception("uncontracting....,custmr busi update fail,custmrbuis acnt_no="+custmrBusi.getACNT_NO());
+				 }
+			}
+			 //如果扣款成功则立即发一笔付款
+			paymentResultBean = BpsTransaction.cupsPayment(custmrBusi,order.getORDER_AMT()+"");
+			//付款结果入库
+			webLogService.saveWebLogC(webLogDBean,paymentResultBean);
+			if(BpsTransaction.SUCC_CODE.equals(paymentResultBean.getRespCode())){//付款成功
+				logger.debug("");
+			}else{
+				logger.debug("");
 			}
 		}
 		if(BpsTransaction.SUCC_CODE.equals(deductionResultBean.getRespCode())){
@@ -130,8 +162,6 @@ public class OrderPayWorker implements Runnable {
 				orderService.updateOrderSt(order.getROW_ID(),TDataDictConst.IVR_ORDER_PAY_FAIL,0);//修改订单状态为结束
 			}
 		}
-		//响应
-	   responseVpc(order.getMOBILE_NO());
 	}
 
 	private void responseVpc(String mobile) {
